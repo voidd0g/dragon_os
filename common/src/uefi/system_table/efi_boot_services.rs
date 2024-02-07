b4cff97129fc5603cf8b7512525ceea9fa6f8e1d
voidd0g <voidrideregg@gmail.com>
2024-02-07 10:36:02 +0900

    Adding documentation links

--- common/src/uefi/system_table/efi_boot_services.rs
+++ common/src/uefi/system_table/efi_boot_services.rs
@@ -2,6 +2,8 @@ use crate::uefi::function_types::{EFI_RAISE_TPL, EFI_RESTORE_TPL};
 
 use super::efi_table_header::EFI_TABLE_HEADER;
 
+/// Documentation is on: 
+/// https://uefi.org/specs/UEFI/2.10/04_EFI_System_Table.html?#efi-boot-services
 #[repr(C)]
 pub struct EFI_BOOT_SERVICES { 
 	Hdr: EFI_TABLE_HEADER,
